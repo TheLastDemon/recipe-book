c1b3c02f79a176e81da35b875e5880f022700aa2
TheLastDemon <d.kurackin675@yandex.ru>
2023-02-24 22:20:45 +0300

    time checker

--- app/controllers/recipes_controller.py
+++ app/controllers/recipes_controller.py
@@ -11,9 +11,16 @@ recipes = Blueprint(name=__name__.split('.')[-1], import_name=__name__)
 @recipes.route('/get-recipes', methods=['GET'])
 @cache.cached(timeout=30, query_string=True)
 def get_all_recipes():
+    entrypoint = datetime.datetime.now()
+    print('entrypoint: ', entrypoint)
+
     data = recipes_service.get_all_recipes()
 
     response = jsonify(data)
+
+    exitpoint = datetime.datetime.now()
+    print('exitpoint: ', exitpoint)
+    print('diff: ', (exitpoint-entrypoint).microseconds)
 
     return response, 200
 
@@ -21,17 +28,28 @@ def get_all_recipes():
 @recipes.route('/get-recipe-by-recipe-id', methods=['POST'])
 @cache.cached(timeout=30, query_string=True)
 def get_recipe_by_id():
+    entrypoint = datetime.datetime.now()
+    print('entrypoint: ', entrypoint)
+
     recipe_id = request.json.get('recipe_id', None)
 
     data = recipes_service.get_recipe_by_id(recipe_id)
 
     response = jsonify(data)
 
+    exitpoint = datetime.datetime.now()
+    print('exitpoint: ', exitpoint)
+    print('diff: ', (exitpoint-entrypoint).microseconds)
+
     return response, 200
 
 
 @recipes.route('/create-recipe', methods=['POST'])
+@cache.cached(timeout=30, query_string=True)
 def create_recipe():
+    entrypoint = datetime.datetime.now()
+    print('entrypoint: ', entrypoint)
+
     name = request.json.get('name', None)
     description = request.json.get('description', None)
     recipe_time = request.json.get('recipe_time', None)
@@ -42,5 +60,9 @@ def create_recipe():
     data = recipes_service.create_recipe(name, description, recipe_time, calories)
 
     response = jsonify(data)
+
+    exitpoint = datetime.datetime.now()
+    print('exitpoint: ', exitpoint)
+    print('diff: ', (exitpoint-entrypoint).microseconds)
 
     return response, 200
--- main.py
+++ main.py
@@ -3,11 +3,3 @@ import redis
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3057, debug=True)
-
-    # r = redis.StrictRedis(
-    #     host="c-c9q390bb2d0v3okhrc2c.rw.mdb.yandexcloud.net",
-    #     password="rezina675",
-    # )
-    #
-    # r.set("foo", "bar")
-    # print(r.get("foo"))
